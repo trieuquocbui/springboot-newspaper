d4f98ba4581e05edad4c0b4472edbed51b6f5bbb
BuiQuocTrieu <buiquoctrieu020402@gmail.com>
2024-06-09 10:31:17 -0700

    delete favourite-service

--- favourite-service/src/main/java/com/bqt/newspaer/exception/EntityAlreadyException.java
+++
@@ -1,7 +0,0 @@
-package com.bqt.newspaer.exception;
-
-public class EntityAlreadyException extends NewsPaperGlobalException {
-    public EntityAlreadyException(String message, String code) {
-        super(code, message);
-    }
-}
